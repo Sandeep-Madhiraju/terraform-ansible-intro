ab927a8f9225b61be808ff66df9b8e9a7009fad3
Sandeep-Madhiraju <87055139+Sandeep-Madhiraju@users.noreply.github.com>
2021-09-28 20:09:32 +0530

    Update gcp_variables.tf

--- deployment/gcp_variables.tf
+++ deployment/gcp_variables.tf
@@ -21,24 +21,24 @@
 
 variable "gcp_project_id" {
   description = "GCP Project ID"
-  type = "string"
+  type = string
   default = ""
 }
 
 variable "gcp_region" {
   description = ""
-  type = "string"
+  type = string
   default = ""
 }
 
 variable "gcp_credentials_file" {
   description = "Full path to the JSON credentials file"
-  type = "string"
+  type = string
   default = "../gcp_compute_key.json"
 }
 
 variable "gcp_ssh_key" {
     description = "Full path to the SSH public key file"
-    type = "string"
+    type = string
     default = "../../.ssh/lab_ssh_key.pub"
 }
